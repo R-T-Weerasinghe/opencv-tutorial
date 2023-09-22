62afe5113ace8442bd7f2e198526fda9fc711dc2
ravindu <ravindutharuka2001@gmail.com>
2023-09-22 09:54:27 +0530

    tutorial 3 added

--- tutorial1.py
+++ tutorial1.py
@@ -2,15 +2,15 @@ import cv2
 from random import randint
 
 img = cv2.imread('assets/dog.jpg', 1)
-img = cv2.resize(img, (500, 500))
-winname = "Image"
+img = cv2.resize(img, (600, 400))
+winName = "Image"
 
 for i in range(100):
     for j in range(img.shape[1]):
         img[i][j] = [randint(0, 255), randint(0, 255), randint(0, 255)]
 
-cv2.namedWindow(winname)
-cv2.moveWindow(winname, 40, 30)
-cv2.imshow(winname, img)
+cv2.namedWindow(winName)
+cv2.moveWindow(winName, 40, 30)
+cv2.imshow(winName, img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
